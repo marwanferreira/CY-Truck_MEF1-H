b240fc3ff46c326727961079bef148196044ae21
Relkha <132769512+Relkha@users.noreply.github.com>
2024-01-17 10:44:23 +0100

    Update l-for-test.sh

--- l-for-test.sh
+++ l-for-test.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-CSV_FILE="data.csv"
+# Nom du fichier CSV
+INPUT_FILE="data.csv"
 
-if [ ! -f "$CSV_FILE" ]; then
-    echo "Le fichier $CSV_FILE n'existe pas."
+# Vérifier que le fichier existe
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Le fichier $INPUT_FILE n'existe pas."
     exit 1
 fi
-
+# Début du chronomètre
 start_time=$(date +%s)
+# Calculer la somme des distances pour chaque trajet
+# Note : La distance est dans la 5ème colonne, l'identifiant de la route est dans la 1ère colonne
+awk -F';' '{ distance[$1] += $5 } END { for (trajet in distance) print trajet, distance[trajet] }' "$INPUT_FILE" |
+    sort -nrk2 | # Tri par distance décroissante
+    head -n 10 |  # Garder les 10 premières lignes
+    sort -nrk1   # Tri final par identifiant de trajet croissant
 
-awk -F ';' 'NR > 1 {gsub(",", ".", $5); sum[$1] += $5} END {for (i in sum) print sum[i], i}' "$CSV_FILE" | sort -nrk1 | head -10 
-
+# Fin du chronomètre
 end_time=$(date +%s)
+
+# Calcul de la durée d'exécution
 echo "Durée d'exécution: $((end_time - start_time)) secondes."
